7baf2dc07e7201a100afb9c3cd2dccc12fbc54b8
HouNingZhou <allcky@qq.com>
2016-02-18 19:33:45 +0800

    修改 bootstrap

--- js/hnz-index.js
+++ js/hnz-index.js
@@ -1,8 +1,12 @@
 $(function(){
+	//导航 小屏 点击按钮显示 菜单列表
 	var minNavMore=$('.min-nav-more');
 	var minNavList=$('.min-nav-list');
 	minNavMore.on('touchstart',function(){
 		$(this).toggleClass("active");
 		minNavList.finish().slideToggle("200");
 	})
+	//banner
+	
+
 })
