b91531bc017d5029b39b90c6818cf616ba38c95e
李星 <1459294281@qq.com>
2016-02-21 21:26:15 +0800

    student
    need help

---
+++ js/lx_student.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Administrator on 2016/2/18.
+ */
+$(function(){
+    //底部翻页宽度
+    var widthA=$(".lx_pageNow").outerWidth(true);
+    var lengthA=$(".lx_pageTurn a").length-1;
+    var widths=widthA*lengthA+140
+    $(".lx_pageTurn").css("width",widths);
+
+    var widthA=$(".lx_pageNow1").outerWidth(true);
+    var lengthA=$(".lx_pageTurn1 a").length-1;
+    var widths=widthA*lengthA+140
+    $(".lx_pageTurn1").css("width",widths);
+
+
+})
