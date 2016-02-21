504c028c82f1d3a2f12af696b6da9399e13bf90a
wangqiccc <291315268@qq.com>
2016-02-21 23:51:14 +0800

    修改遮罩效果

--- js/wq-xszp.js
+++ js/wq-xszp.js
@@ -7,7 +7,7 @@ $(function(){
 	var windowheight=$(window).height()
   var windowwidth=$(window).width()
  $(".wq-imgbox").click(function(){
-		if(windowwidth>640){
+		if(windowwidth>768){
     var mask=$("<div>").css({
 			width:"100%",
 			minHeight:windowheight+"px",
@@ -16,7 +16,16 @@ $(function(){
 			left:0,
 			top:0,
       textAlign:"center"
-		})
+		})}else{
+    var mask=$("<div>").css({
+      width:"100%",
+      background:"rgba(0,0,0,0.5)",
+      position:"absolute",
+      left:0,
+      top:0,
+      textAlign:"center"
+    })  
+    }
 	  $("body").append(mask)
     var src=$(this).attr("data-src")
     var img=$("<img>").attr("src",src).css("maxWidth","80%")
@@ -28,22 +37,34 @@ $(function(){
           e.preventDefault()
           else
           e.returnValue="false"
+          
           var top=parseInt(mask.css("top"))
+          if(top<0){
           top+=value;
           mask.css("top",top+"px")
+          }else{
+            mask.css("top",0) 
+          }
 	    },function(e){
           if(e.preventDefault)
           e.preventDefault()
           else
           e.returnValue="false"
+       
           var top=parseInt(mask.css("top"))
+          var maskheight=parseInt(mask.css("height"))
+          if(top>windowheight-maskheight){
           top-=value;
-          mask.css("top",top+"px")	
+          mask.css("top",top+"px")
+          }else{
+           top=windowheight-maskheight
+           mask.css("top",top+"px")
+          }
 	    })
      mask.click(function(){
       $(this).remove()
      }) 
-    }
+
 	}) 
 function mousewheel(obj,upfun,downfun){
     if(obj.addEventListener){
